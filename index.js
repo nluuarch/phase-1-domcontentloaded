b35451e0362b7487db565af88bfcd2d5731435f0
nluuarch@gmail.com <nluuarch@gmail.com>
2021-11-10 15:27:13 -0800

    commit tree 90f830725632dfd78bac9e00d2febbd3e5ac4e2c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function getText() {
+    console.log("The DOM has loaded")
+});
+
+function getText () {
+    let text = document.getElementById('text');
+    text.innerText = "This is really cool!"
+}
